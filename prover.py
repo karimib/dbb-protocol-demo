70e7ba545443f4a171b6674ff6e036b60d395800
Karim Ibrahim <karim.ibrahim93@gmail.com>
2024-04-27 19:08:29 +0200

    Implement hancke kuhn

--- prover.py
+++ prover.py
@@ -1,14 +1,17 @@
+import random
 import socket
 import os
 import hmac
 import hashlib
 import time
 
+secret_key = b'supersecretkey'
+iterations = 10
+
 
 def main():
     host = 'localhost'
     port = 18080
-    secret_key = b'supersecretkey'
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
@@ -18,29 +21,30 @@ def main():
     conn, addr = server_socket.accept()
     print(f"Connected by {addr}")
 
+    print("Start: Setup Phase")
+    verifier_nonce = conn.recv(128)
+    print("Received nonce N_v: ", verifier_nonce)
+    prover_nonce = os.urandom(16)
+    conn.sendall(prover_nonce)
+    print("Sent nonce: N_p: ", prover_nonce)
+    random.seed(secret_key + verifier_nonce + prover_nonce)
+    shared_bits = ''.join(str(random.randint(0, 1)) for _ in range(2*iterations))
+    print("Shared bits:", shared_bits)
+    print("End: Setup Phase")
+
     try:
-        while True:
-            data = conn.recv(128)
-            if not data:
-                break
-
+        for i in range(iterations):
+            data = conn.recv(1)
             # Extract nonce and challenge
-            nonce, challenge = data[:16], data[16:]
-            print("Received nonce and challenge:", nonce, challenge)
-            response = generate_response(challenge, secret_key)
-            hmac_result = hmac.new(secret_key, nonce + challenge + response, hashlib.sha256).digest()
-
-            # Send response and HMAC
-            conn.sendall(response + hmac_result)
-            print("Sent response and HMAC")
+            challenge = int(data.decode())
+            print("Received challenge:", challenge)
+            r_i = shared_bits[(2 * i + challenge - 1)]
+            print("Generated response:", r_i)
+            conn.send(str(r_i).encode())
 
     finally:
         conn.close()
         server_socket.close()
-
-
-def generate_response(challenge, secret_key):
-    return bytes([challenge[i] ^ secret_key[i % len(secret_key)] for i in range(len(challenge))])
 
 
 if __name__ == "__main__":
--- verifier.py
+++ verifier.py
@@ -1,39 +1,59 @@
+import random
 import socket
 import os
 import hmac
 import hashlib
 import time
 
+secret_key = b'supersecretkey'
+iterations = 10
+
+
 def main():
     host = 'localhost'
     port = 18080
-    secret_key = b'supersecretkey'
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
+    wait = input("Press Enter to continue...")
+
+    print("Start: Setup Phase")
+    verifier_nonce = os.urandom(16)
+    client_socket.sendall(verifier_nonce)
+    print(">Sent nonce N_v: ", verifier_nonce)
+    prover_nonce = client_socket.recv(128)
+    print("<Received nonce N_p: ", prover_nonce)
+    random.seed(secret_key + verifier_nonce + prover_nonce)
+    shared_bits = ''.join(str(random.randint(0, 1)) for _ in range(2*iterations))
+    print("Shared bits:", shared_bits)
+    print("End: Setup Phase")
+
     try:
-        for i in range(10):  # 10 challenge-response rounds
-            nonce = os.urandom(16)
-            challenge = os.urandom(16)
-            print("Sending nonce and challenge:", nonce, challenge)
+        for i in range(iterations):  # 10 challenge-response rounds
+            c_i = random.randint(0, 1)
+            print(">Sending challenge:", c_i)
+
             start_time = time.perf_counter_ns()
-            client_socket.sendall(nonce + challenge)
-
-            data = client_socket.recv(128)
+            client_socket.send(str(c_i).encode())
+            data = client_socket.recv(1)
             end_time = time.perf_counter_ns()
-            response, received_hmac = data[:len(challenge)], data[len(challenge):]
-            expected_hmac = hmac.new(secret_key, nonce + challenge + response, hashlib.sha256).digest()
+            expected_response = shared_bits[(2 * i + c_i - 1)]
+            print("<Received response:", data)
+            print("Expected response:", expected_response)
+            response = str(int(data.decode()))
 
-            if hmac.compare_digest(received_hmac, expected_hmac):
-                print("Valid response received")
+
+            if expected_response == response:
+                print("<Valid response received")
             else:
-                print("Invalid response or tampering detected")
+                print("<Invalid response or tampering detected")
 
-            print("Round trip time:", (end_time - start_time) / 1e6, "ms")
+            print("=Round trip time:", (end_time - start_time) / 1e6, "ms")
 
     finally:
         client_socket.close()
+
 
 if __name__ == "__main__":
     main()
